60e3ca8e36ce0b79d533732645b21fc942eec56b
Iurii Pototskii <iuriipototskii@gmail.com>
2022-09-29 10:26:19 +0000

    fix: naming

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandom(x, y) {
+function getRandomInt(x, y) {
   if (Number.isFinite(x) && Number.isFinite(y) && x >= 0 && y >= 0) {
     // if (x === y) {
     //   return Number.isInteger(x) ? x : NaN;
@@ -10,8 +10,8 @@ function getRandom(x, y) {
 
     const min = Math.ceil(Math.min(x, y));
     const max = Math.floor(Math.max(x, y));
-    const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-    return (randomNum >= min && randomNum <= max) ? randomNum : null;
+    const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+    return (randomInt >= min && randomInt <= max) ? randomInt : null;
   }
 
   return NaN;
