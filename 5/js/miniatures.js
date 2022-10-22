c3b8528f1b9bfcfdf4145f457152db4d89d5965d
Keks <keks@htmlacademy.ru>
2022-10-22 15:10:11 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/miniatures.js
@@ -0,0 +1,19 @@
+import {getPhotosArr} from './generate-data.js';
+
+const photoContainer = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const photosArr = getPhotosArr();
+const photosListFragment = document.createDocumentFragment();
+
+photosArr.forEach(({url, likes, comments}) => {
+  const photo = photoTemplate.cloneNode(true);
+  photo.querySelector('.picture__img').src = url;
+  photo.querySelector('.picture__comments').textContent = comments.length;
+  photo.querySelector('.picture__likes').textContent = likes;
+  photosListFragment.append(photo);
+});
+
+photoContainer.append(photosListFragment);
