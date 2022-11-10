8fe8a1f3ae6cc513083ced4b198ddd53f143d87b
Iurii Pototskii <iuriipototskii2gmail.com>
2022-11-10 21:26:54 +0200

    refactor: more universal scaling callbacks

--- js/img-upload-effect-filter.js
+++ js/img-upload-effect-filter.js
@@ -1,62 +1,10 @@
-const imgScaleInput = document.querySelector('.scale__control--value');
 const previewImage = document.querySelector('.img-upload__preview > img');
+const effectSliderWrap = document.querySelector('.effect-level__slider');
+const effectLevelInput = document.querySelector('.effect-level__value');
 
 let currentEffect = 'effects__preview--none';
 let currentStyle = '';
 let styleUnits = '';
-
-const effectSliderWrap = document.querySelector('.effect-level__slider');
-const effectLevelInput = document.querySelector('.effect-level__value');
-
-// scale effects
-
-function upScale() {
-  switch (imgScaleInput.value) {
-    case '25%':
-      imgScaleInput.value = '50%';
-      previewImage.classList.remove('img-upload__preview-scale25');
-      previewImage.classList.add('img-upload__preview-scale50');
-      break;
-    case '50%':
-      imgScaleInput.value = '75%';
-      previewImage.classList.remove('img-upload__preview-scale50');
-      previewImage.classList.add('img-upload__preview-scale75');
-      break;
-    case '75%':
-      imgScaleInput.value = '100%';
-      previewImage.classList.remove('img-upload__preview-scale75');
-      previewImage.classList.add('img-upload__preview-scale100');
-      break;
-    default:
-      imgScaleInput.value = '100%';
-      previewImage.classList.add('img-upload__preview-scale100');
-  }
-}
-
-function downScale() {
-  switch (imgScaleInput.value) {
-    case '100%':
-      imgScaleInput.value = '75%';
-      previewImage.classList.remove('img-upload__preview-scale100');
-      previewImage.classList.add('img-upload__preview-scale75');
-      break;
-    case '75%':
-      imgScaleInput.value = '50%';
-      previewImage.classList.remove('img-upload__preview-scale75');
-      previewImage.classList.add('img-upload__preview-scale50');
-      break;
-    case '50%':
-      imgScaleInput.value = '25%';
-      previewImage.classList.remove('img-upload__preview-scale50');
-      previewImage.classList.add('img-upload__preview-scale25');
-      break;
-    default:
-      imgScaleInput.value = '25%';
-      previewImage.classList.add('img-upload__preview-scale25');
-  }
-}
-
-// filter effects
 
 function toggleEffect(evt) {
   if (evt.target.classList.contains('effects__preview')) {
@@ -161,4 +109,4 @@ function toggleEffect(evt) {
   }
 }
 
-export {upScale, downScale, toggleEffect};
+export {toggleEffect};
