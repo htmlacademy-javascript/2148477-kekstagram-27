a8febfac30e5c392941107de930906c5a0d5ab7e
Iurii Pototskii <iuriipototskii2gmail.com>
2022-11-18 19:58:34 +0200

    feat: shown miniatures ordering and filtering

--- js/photo.js
+++ js/photo.js
@@ -1,12 +1,18 @@
 import {getPhotosArr} from './data-download.js';
 import {getBigPicture} from './big-picture.js';
 import {getMiniature} from './miniatures.js';
-import {showAlert} from './util.js';
+import {showAlert, getRandomInt} from './util.js';
+import {getUniqValue} from './cache.js';
+
+const CHOSEN_PHOTOS_RANDOM_LENGTH = 10;
 
 const photoContainer = document.querySelector('.pictures');
+const photoContainerClear = photoContainer.innerHTML;
 const miniatureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
+const chosenPhotosInterface = document.querySelector('.img-filters');
+let currentChosenButton = document.querySelector('.img-filters__button--active');
 
 function getPhotosListFragment (photosArr) {
   const photosListFragment = document.createDocumentFragment();
@@ -25,8 +31,42 @@ function getPhotosListFragment (photosArr) {
   return photosListFragment;
 }
 
-function showPhotos(photos) {
-  photoContainer.append(getPhotosListFragment(photos));
+function showChosenPhotos(photos) {
+  let chosenPhotos = [];
+  // photoContainer.querySelectorAll('a.picture').forEach((elem) => elem.remove());
+
+  photoContainer.innerHTML = photoContainerClear;
+
+  if (currentChosenButton.id === 'filter-default') {
+    photoContainer.append(getPhotosListFragment(photos));
+  } else if (currentChosenButton.id === 'filter-random') {
+    const getRandomPhoto = getUniqValue(getRandomInt, 0, photos.length - 1);
+    for (let i = 0; i < CHOSEN_PHOTOS_RANDOM_LENGTH; i++) {
+      chosenPhotos.push(photos[getRandomPhoto()]);
+    }
+    photoContainer.append(getPhotosListFragment(chosenPhotos));
+  } else if (currentChosenButton.id === 'filter-discussed') {
+    chosenPhotos = photos
+      .slice()
+      .sort((a, b) => b.comments.length - a.comments.length);
+    photoContainer.append(getPhotosListFragment(chosenPhotos));
+  }
 }
 
-getPhotosArr((photosArr) => showPhotos(photosArr), () => {showAlert('Не удалось загрузить фотографии');});
+function setChosenPhotos(cb) {
+  chosenPhotosInterface.addEventListener('click', (evt) => {
+    currentChosenButton.classList.remove('img-filters__button--active');
+    currentChosenButton = evt.target;
+    currentChosenButton.classList.add('img-filters__button--active');
+    cb();
+  });
+}
+
+getPhotosArr(
+  (photosData) => {
+    chosenPhotosInterface.classList.remove('img-filters--inactive');
+    showChosenPhotos(photosData);
+    setChosenPhotos(() => showChosenPhotos(photosData));
+  },
+  () => showAlert('Не удалось загрузить фотографии'),
+);
